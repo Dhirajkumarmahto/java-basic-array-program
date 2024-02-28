7e020ce687d747f78d34420e742028e0a4b47cb2
Dhiraj Kumar <161637836+Dhirajkumarmahto@users.noreply.github.com>
2024-02-29 00:21:34 +0530

    Update sumofoddevenarray.java

--- sumofoddevenarray.java
+++ sumofoddevenarray.java
@@ -1,21 +1,18 @@
 public class sumofoddevenarray {
-    public static void main(String[] args){
-        int[] a={10,20,30,};
-        int[] a1=new int[a.length];
-        int i;
-        int sum=0;
-        for(i=0; i<a.length; i++){
-            sum=sum+a[i];
-        }
-        System.out.print(sum);
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30};
+        int sum = 0;
 
-        if(sum%2==0){
-            sum=a[i]/a.length;
-            System.out.print("even number");
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
         }
-        else{
-            System.out.print("odd numbers");
+        System.out.println("Sum of the array: " + sum);
+
+        if (sum % 2 == 0) {
+            System.out.println("Even sum");
+        } else {
+            System.out.println("Odd sum");
         }
     }
-
 }
+
