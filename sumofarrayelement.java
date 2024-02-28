11cb267f3511957298d76172d6c1895378a9c476
Dhiraj Kumar <161637836+Dhirajkumarmahto@users.noreply.github.com>
2024-02-29 00:17:58 +0530

    Add files via upload

---
+++ sumofarrayelement.java
@@ -0,0 +1,8 @@
+public class sumofarrayelement {
+    public static void main(String[] arga){
+        int a[]={2,4,6,8,7,9,11};
+        int a1[]=new int[a.length];
+        int i;
+        int sum=0;
+
+}
